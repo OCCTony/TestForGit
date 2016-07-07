61d96d10d40ea7faab27dea66b084ac1ad6eb3f9
OCCTony <419224509@qq.com>
2016-07-07 15:22:44 +0800

    Create a file test

---
+++ src/test/Test.java
@@ -0,0 +1,11 @@
+package test;
+
+/**
+ * Created by OCCTony on 2016/7/7.
+ * This is a test for git
+ */
+public class Test {
+    public static void main(String args[]){
+        System.out.print("This is a test ");
+    }
+}
