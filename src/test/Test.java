a868e0cb9f35e27622b547a00c97ce662317c3da
OCCTony <419224509@qq.com>
2016-07-07 15:37:31 +0800

    Changed for second time

--- src/test/Test.java
+++ src/test/Test.java
@@ -3,9 +3,11 @@ package test;
 /**
  * Created by OCCTony on 2016/7/7.
  * This is a test for git
+ * I changed for the second time
  */
 public class Test {
     public static void main(String args[]){
         System.out.print("This is a test ");
+
     }
 }
